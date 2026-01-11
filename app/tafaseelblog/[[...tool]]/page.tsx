21cf412e80474a2c5e24eb70b57c7f14b1c276b1
raghoooood <raghad.nakshou@gmail.com>
2026-01-11 20:49:08 +0300

    tool

--- app/tafaseelblog/[[...tool]]/page.tsx
+++ app/tafaseelblog/[[...tool]]/page.tsx
@@ -1,5 +1,5 @@
 import { NextStudio } from "next-sanity/studio";
-import config from "@/lib/sanity/studioConfig";
+import config from "@/sanity.config";
 
 export default function StudioPage() {
   return <NextStudio config={config} />;
